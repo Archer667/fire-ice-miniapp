1213e162913db725c9803c7aeb2426debc3ad4fd
Arian Naseri <ariannaseri1@gmail.com>
2026-07-06 15:16:20 +0330

    Remove all repository contents

--- src/components/Toast.jsx
+++
@@ -1,6 +0,0 @@
-import { useGame } from '../store.jsx';
-
-export default function Toast() {
-  const { toastMsg, toastVisible } = useGame();
-  return <div className={`toast ${toastVisible ? 'show' : ''}`}>{toastMsg}</div>;
-}
